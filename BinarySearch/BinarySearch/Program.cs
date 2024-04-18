b4f09681699acc2cf4907f3716fca88b2963712e
olegzig <boris37544@gmail.com>
2024-04-18 16:24:58 +0300

    init?

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[size];
+            List<int> list;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = 1;
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+
+            }
+        }
+    }
+}
