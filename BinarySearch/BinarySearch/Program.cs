b1c8515bb3129fa07b14550d6904eba9915f9ddd
olegzig <boris37544@gmail.com>
2024-04-18 17:40:04 +0300

    Сделал бинарный поиск, больше отладки

--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -15,8 +15,6 @@ namespace BinarySearch
             string input = Console.ReadLine();
             int.TryParse(input, out int size);
             SearchableArray array = new SearchableArray(size);
-
-            array.Print();
         }
     }
 
@@ -29,6 +27,15 @@ namespace BinarySearch
             array = new int[size == 0 ? 10 : size];
             FillArray();
             ShowPositiveMessage("Создание успешно! Размер: " + array.Length);
+
+            if(array.Length <= 1000)
+            {
+                Print();
+            }
+            else
+            {
+                ShowInfoMessage("Так как размер больше 1000, отображатся массив не будет.");
+            }
         }
 
         private void FillArray()
@@ -50,6 +57,32 @@ namespace BinarySearch
             Console.WriteLine(']');
         }
 
+        public int FindPositionViaBinarySearch(int elementValue)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            int index = 0;
+
+            while (left <= right)
+            {
+                index = (right + left) / 2;
+                if (array[index] == elementValue)
+                {
+                    return index;
+                } 
+
+                if (array[index] < elementValue)
+                {
+                    left = index + 1;
+                }
+                else
+                {
+                    right = index - 1;
+                }
+            }
+            return -1;
+        }
+
         private static void ShowPositiveMessage(string message)
         {
             Console.ForegroundColor = ConsoleColor.Green;
@@ -57,9 +90,9 @@ namespace BinarySearch
             Console.ResetColor();
         }
 
-        private static void ShowNegativeMessage(string message)
+        private static void ShowInfoMessage(string message)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(message);
             Console.ResetColor();
         }
